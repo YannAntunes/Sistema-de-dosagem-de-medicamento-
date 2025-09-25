928f22187494949e41015359d11ce9a35ae65f67
Yann_Antunes <yannantuneslopes123@gmail.com>
2025-09-25 11:58:29 -0300

    criando um menu para o sistema

--- SitemaDosagem.java
+++ SitemaDosagem.java
@@ -11,37 +11,57 @@ public class SitemaDosagem {
 
         List<RegistroDosagem> historico = new ArrayList<>();
 
-        boolean continuar = true;
+        boolean rodando = true;
 
-        while (continuar) {
-            Paciente paciente = ui.criarPaciente();
-            if (paciente == null) break;
+        while (rodando) {
 
-            Medicamento medicamento = ui.criarMedicamento();
-            if (medicamento == null) break;
+            System.out.println("\n=======================");
+            System.out.println("  SISTEMA DE DOSAGEM MEDICA");
+            System.out.println("=========================");
+            System.out.println("1 - Nova calculo");
+            System.out.println("2 - Ver historico");
+            System.out.println("3 - Sair");
+            System.out.println("Escolha uma opcao: ");
 
-            double doseFinal = CalculadoraDosagem.calcular(paciente, medicamento);
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
 
-            ui.mostrarResultado(paciente, medicamento, doseFinal);
+            switch (opcao) {
+             case 1:
+                Paciente paciente = ui.criarPaciente();
+                if (paciente == null) break;
 
-            historico.add(new RegistroDosagem(paciente, medicamento, doseFinal));
+                Medicamento medicamento = ui.criarMedicamento();
+                if (medicamento == null) break;
 
-            System.out.println("\nDeseja calcular outro paciente? (s/n): ");
-            String resposta = scanner.nextLine();
-            if (resposta.equalsIgnoreCase("n")) {
-                continuar = false;
+                double doseFinal = CalculadoraDosagem.calcular(paciente, medicamento);
+
+                ui.mostrarResultado(paciente, medicamento, doseFinal);
+
+                historico.add(new RegistroDosagem(paciente, medicamento, doseFinal));
+                break;
+            
+            case 2:    
+                System.out.println("\n==== Historico de Atendimentos ====");
+                if (historico.isEmpty()) {
+                    System.out.println("Nenhum atendimento registrado");
+                } else {
+                    for (int i = 0; i < historico.size(); i++) {
+                        RegistroDosagem r = historico.get(i);
+                        System.out.println((i+1) + ", Paciente: " + r.getPaciente().getNome() + " | Remedio: " + r.getMedicamento().getNome() + " | Dosagem: " + r.getDoseFinal() + " mg");
+                    }
+                }
+                break;
+
+            case 3: 
+                rodando = false;
+                System.out.println("\nEncerrando o sistema.");
+                break;
+
+                default:
+                System.out.println("Opcao invalida! Escolha 1, 2, ou 3");
             }
         }
 
-        System.out.println("\n==== Relatorio de Atendimentos ====");
-        for (RegistroDosagem r : historico) {
-            System.out.println(
-                "Paciente " + r.getPaciente().getNome() +
-                " | Medicamento: " + r.getMedicamento().getNome() +
-                " | Dosagem: " + r.getDoseFinal() + " mg"
-            );
-        }
-
-        System.out.println("Encerrando Sistema");
     }
 }
